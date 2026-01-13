b76d71062c5f1088be6135d93d6785519ca0ddf8
Jim Walejko <jwalejko@me.com>
2026-01-12 21:30:33 -0700

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Jim Walejko on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
